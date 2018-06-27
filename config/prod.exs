654c63f9fb7ff11d2958667ca20b108abc9d56ad
Timothy Stapleton <retrocede@outlook.com>
2018-06-27 07:01:01 -0700

    update production config to use heroku vars

--- config/prod.exs
+++ config/prod.exs
@@ -15,11 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :movie_night, MovieNightWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "https://glacial-earth-44348.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+#config database
+config :movie_night, MovieNight.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -61,4 +69,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
